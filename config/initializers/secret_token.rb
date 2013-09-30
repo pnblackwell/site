f8c2dbbbe8b55c7ab2bb8192c959df4162c23d89
Paul Blackwell <Paul@Pauls-MacBook-Air.local>
2013-09-30 18:12:59 -0400

    init rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Site::Application.config.secret_key_base = '81f75de1d2e64a216c48135e040d02dd89d87bf7e0d1f679a2c3271d5b09ff6feb9fa72bff10d4eb5c4e7a6e330582366f1ccb84f5eabb2a94c86a6b0246fe29'
---
+++ db/migrate/20130830230924_add_title_and_bodyto_articles.rb
@@ -0,0 +1,4 @@
+class AddTitleAndBodytoArticles < ActiveRecord::Migration
+  def change
+  end
+end
